7ccc87576344db513387bc85133de60c2c4dcc31
DenzDy <dydenzellrobyn0527@gmail.com>
2024-11-07 20:58:04 +0800

    Feature: printing of game board in C

--- main.c
+++ main.c
@@ -2,12 +2,26 @@
 #include "stdlib.h"
 #include "string.h"
 #include "math.h"
+void print_game_board(int board[][3]){
+    for(int i = 0; i < 3; i++){
+        printf("+---+---+---+\n");
+        for(int j = 0; j < 3; j++){
+            if(board[i][j] == 0){
+                printf("|   ");
+            }
+            else{
+                printf("| %d ", board[i][j]);
+            }
+        }
+        printf("|\n");
+    }
+    printf("+---+---+---+\n");
+}
 
 int main(){
-    
+    int game_board[3][3] = {{0,0,0}, {0,0,0}, {0,0,0}};
     printf("Choose [1] or [2]: \n[1] New Game\n[2] Start from a State\n");
     int new_game_input;
     scanf("%d", &new_game_input);
-    
-
+    print_game_board(game_board);
 }
