2e23568ffc5523fa345b07f17abd0e82261a50c6
DenzDy <dydenzellrobyn0527@gmail.com>
2024-11-07 20:31:27 +0800

    Create C file

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "math.h"
+
+int main(){
+    
+    printf("Choose [1] or [2]: \n[1] New Game\n[2] Start from a State\n");
+    int new_game_input;
+    scanf("%d", &new_game_input);
+    
+
+}
